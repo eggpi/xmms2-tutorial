8061a6997cda8aead1ed208257ad2781a750dac0
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-07-15 12:00:56 -0300

    OTHER: Add low-level service clients C tutorials.

---
+++ c/tut10.c
@@ -0,0 +1,123 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  This file implements the low-level 'sumclient' client, which
+ *  asynchronously interacts with 'sumservice' (implemented in tut9.c).
+ *  It sends a message containing two operands in a list to another client
+ *  and expects their sum as a reply. The destination client's id and the
+ *  two operands are specified in the command line.
+ *  Should be run/studied along with tut9 aka sumservice.
+ */
+
+#include <stdlib.h>
+#include <glib.h>
+#include <xmmsclient/xmmsclient.h>
+#include <xmmsclient/xmmsclient-glib.h>
+
+/* Callback for the reply from the other client.
+ * This receives a client-to-client reply, which we expect to
+ * contain an integer (the sum of the operands we sent), parses
+ * this integer and prints it out.
+ */
+gint
+print_sum (xmmsv_t *c2c_reply, void *udata)
+{
+	gint sum;
+	const gchar *err;
+
+	if (xmmsv_get_error (c2c_reply, &err)) {
+		fprintf (stderr, "ERROR: %s\n", err);
+		goto out;
+	}
+
+	/* Since we used xmmsc_result_notifier_set when setting up
+	 * this callback in the main funcion, it only receives the payload
+	 * of the reply, with no metadata. This means 'c2c_reply' is simply a
+	 * xmmsv_t integer, which we parse as such.
+	 * In contrast, we could have used xmmsc_result_notifier_set_raw
+	 * or xmmsc_result_notifier_set_c2c, in which case the contents of
+	 * 'c2c_reply' would be different.
+	 * Please consult the documentation for those functions for more
+	 * information.
+	 */
+	if (!xmmsv_get_int (c2c_reply, &sum)) {
+		fprintf (stderr, "Can't get an integer out of the reply.\n");
+		goto out;
+	}
+
+	printf ("Sum is %d\n", sum);
+
+out:
+	/* Return value for this callback doesn't matter, it's not
+	 * a broadcast nor a signal.
+	 * We leave the mainloop and exit the client.
+	 */
+	g_main_loop_quit ((GMainLoop *) udata);
+	return TRUE;
+}
+
+int
+main (int argc, char **argv)
+{
+	GMainLoop *ml;
+	xmmsv_t *operands;
+	xmmsc_result_t *result;
+	xmmsc_connection_t *connection;
+
+	if (argc != 4) {
+		fprintf (stderr, "Usage: %s service-id op1 op2\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* Connect as usual. */
+	connection = xmmsc_init ("sumclient");
+	if (!connection) {
+		fprintf (stderr, "OOM!\n");
+		exit (EXIT_FAILURE);
+	}
+
+	if (!xmmsc_connect (connection, getenv ("XMMS_PATH"))) {
+		fprintf (stderr, "Connection failed: %s\n",
+		         xmmsc_get_last_error (connection));
+
+		exit (EXIT_FAILURE);
+	}
+
+	ml = g_main_loop_new (NULL, FALSE);
+
+	/* Build a list of two operands for sumservice (tut9). */
+	operands = xmmsv_build_list (xmmsv_new_int (atoi(argv[2])),
+	                             xmmsv_new_int (atoi(argv[3])),
+	                             XMMSV_LIST_END);
+
+	/* And send the list to the sumservice.
+	 * We use the reply policy of only expecting a single reply,
+	 * which means the receiving client will get an error from
+	 * the server if it attempts to reply to this message more
+	 * than once.
+	 */
+	result = xmmsc_c2c_send (connection,
+	                         atoi(argv[1]),
+	                         XMMS_C2C_REPLY_POLICY_SINGLE_REPLY,
+	                         operands);
+
+	xmmsc_result_notifier_set (result, print_sum, ml);
+	xmmsc_result_unref (result);
+
+	xmmsc_mainloop_gmain_init (connection);
+	g_main_loop_run (ml);
+
+	xmmsc_unref (connection);
+	return EXIT_SUCCESS;
+
+}
---
+++ c/tut9.c
@@ -0,0 +1,147 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  This file implements the low-level 'sumserver' service client.
+ *  It accepts messages containing a list of two operands, and replies
+ *  with the sum of these operands.
+ *  Should be run/studied along with tut10 aka sumclient.
+ */
+
+#include <stdlib.h>
+#include <xmmsclient/xmmsclient.h>
+#include <xmmsclient/xmmsclient-glib.h>
+
+#include <glib.h>
+
+/* Each connected client is uniquely identified by a numeric
+ * id in the server. A client's id should be used in order to
+ * send messages to it.
+ * The server provides a command for a client to consult its own id.
+ * This is a simple command given to the server, no client-to-client
+ * messages are involved just yet.
+ * Here we fetch and print the id assigned by the server to this client.
+ * This should be passed in the command line for tut10.
+ */
+void
+print_id (xmmsc_connection_t *connection)
+{
+	gint32 id;
+	xmmsv_t *idv;
+	xmmsc_result_t *result;
+
+	result = xmmsc_c2c_get_own_id (connection);
+	xmmsc_result_wait (result);
+
+	idv = xmmsc_result_get_value (result);
+	xmmsv_get_int (idv, &id);
+
+	printf ("Connected as id %d.\n"
+	        "Pass this id in the command line for tut10.\n", id);
+
+	xmmsc_result_unref (result);
+	return;
+}
+
+/* Client-to-client messages come with a special broadcast, so
+ * in order to receive them we must set up a callback to that broadcast.
+ * This is the callback for this client. It parses two operands out of
+ * the client-to-client message and replies with their sum.
+ */
+int
+sum (xmmsv_t *c2c_msg, void *userdata)
+{
+	int op1, op2;
+	xmmsv_t *operands, *sum;
+	xmmsc_connection_t *connection;
+
+	connection = (xmmsc_connection_t *) userdata;
+
+	/* Get the payload list of operands from the message.
+	 * It is the payload sent by sumclient.
+	 */
+	operands = xmmsv_c2c_message_payload_get (c2c_msg);
+	if (!operands) {
+		fprintf (stderr, "Can't get operands from c2c message!\n");
+		return TRUE;
+	}
+
+	/* Get the two operands from the list */
+	if (!xmmsv_list_get_int (operands, 0, &op1)) {
+		fprintf (stderr, "Failed to get first operand.\n");
+		return TRUE;
+	}
+
+	if (!xmmsv_list_get_int (operands, 1, &op2)) {
+		fprintf (stderr, "Failed to get second operand.\n");
+		return TRUE;
+	}
+
+	/* Build a new xmmsv with the sum of the operands
+	 * and send it back to sumclient as a reply.
+	 * We specify that we are answering to this particular
+	 * message by passing its id (which we retrieve using
+	 * xmmsv_c2c_message_id_get), and use a reply policy
+	 * of not expecting counter-replies. This means that the
+	 * other client will get an error from the server if it
+	 * attempts to reply to our reply.
+	 */
+	sum = xmmsv_new_int (op1 + op2);
+	xmmsc_c2c_reply (connection,
+	                 xmmsv_c2c_message_id_get (c2c_msg),
+	                 XMMS_C2C_REPLY_POLICY_NO_REPLY,
+	                 sum);
+
+	return TRUE;
+}
+
+int
+main (int argc, char **argv)
+{
+	GMainLoop *ml;
+	xmmsc_result_t *result;
+	xmmsc_connection_t *connection;
+
+	/* Connect as usual */
+	connection = xmmsc_init ("sumservice");
+	if (!connection) {
+		fprintf (stderr, "OOM!\n");
+		exit (EXIT_FAILURE);
+	}
+
+	if (!xmmsc_connect (connection, getenv ("XMMS_PATH"))) {
+		fprintf (stderr, "Connection failed: %s\n",
+		         xmmsc_get_last_error (connection));
+
+		exit (EXIT_FAILURE);
+	}
+
+	/* Output the id assigned to this client. */
+	print_id(connection);
+
+	ml = g_main_loop_new (NULL, FALSE);
+
+	/* Set up the c2c message broadcast.
+	 * This allows us to receive messages sent by other clients to our id.
+	 * We are going to need the connection to the server in order to reply,
+	 * so pass it as user data.
+	 */
+	result = xmmsc_broadcast_c2c_message (connection);
+	xmmsc_result_notifier_set_c2c (result, sum, connection);
+	xmmsc_result_unref (result);
+
+	xmmsc_mainloop_gmain_init (connection);
+	g_main_loop_run (ml);
+
+	xmmsc_unref (connection);
+	return EXIT_SUCCESS;
+}
